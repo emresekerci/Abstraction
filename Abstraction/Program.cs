8ae6af3a5fedcfabe3df8bdbb5d52f30697c6a25
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-14 23:13:24 +0300

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,28 @@
+﻿// Yazılım Geliştirici örneği
+using Abstraction;
+
+Sirket yazilimci = new YazilimGelistirici
+{
+    Ad = "Emre",
+    Soyad = "Şekerci",
+    Departman = "Yazılım Geliştirme"
+};
+yazilimci.Gorev();
+
+// Proje Yöneticisi örneği
+Sirket yonetici = new ProjeYoneticisi
+{
+    Ad = "Hasan",
+    Soyad = "Çıldırmış",
+    Departman = "Proje Yönetimi"
+};
+yonetici.Gorev();
+
+// Satış Temsilcisi örneği
+Sirket satici = new SatisTemsilcisi
+{
+    Ad = "Ayşe",
+    Soyad = "Duran",
+    Departman = "Satış"
+};
+satici.Gorev();
---
+++ Abstraction/Sirket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Sirket
+    {
+        public  string Ad { get; set; }
+        public  string Soyad { get; set; }
+        public  string Departman { get; set; }
+        public abstract void Gorev();
+        
+            
+        
+    }
+    public class YazilimGelistirici : Sirket
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Yazılım Geliştiricisi, yazılım geliştiriyorum.");
+        }
+    }
+
+    // Proje Yöneticisi sınıfı
+    public class ProjeYoneticisi : Sirket
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Proje Yöneticisi, proje yönetiyorum.");
+        }
+    }
+
+    // Satış Temsilcisi sınıfı
+    public class SatisTemsilcisi : Sirket
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Satış Temsilcisi, satış yapıyorum.");
+        }
+    }
+}
